fffd4d6e986493c1766dd211e75e62053981531a
luisfran10 <luicao10@gmail.com>
2021-03-16 18:12:25 -0600

    [FIX] - User constants namespaces was renamed

---
+++ src/modules/users/user/constants/jwt.constant.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_STRATEGY = 'jwt';
+export const TOKEN_EXPIRES = 600;
+export const SALT = 10;
---
+++ src/modules/users/user/constants/user-schema.constant.ts
@@ -0,0 +1,8 @@
+export const NAME_MIN_LENGHT = 6;
+export const NAME_MAX_LENGHT = 30;
+export const PHONE_LENGHT = 14;
+export const EMAIL_PATTERN = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const PHONE_NUMBER_PATTERN = /\d{4}-\d{4}/;
+export const PASSWORD_PATTERN = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+export const PASSWORD_MIN_LENGHT = 6;
+export const PASSWORD_MAX_LENGHT = 10;
