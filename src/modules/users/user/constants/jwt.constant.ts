4847b3643bac68c52865fd54c7d059d3bdd50668
luisfran10 <luicao10@gmail.com>
2021-03-25 11:07:00 -0600

    [FIX] - Purged innecesary constants files

--- src/modules/users/user/constants/jwt.constant.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_STRATEGY = 'jwt';
-export const TOKEN_EXPIRES = 600;
-export const SALT = 10;
--- src/modules/users/user/constants/user-alias.constant.ts
+++
@@ -1,4 +0,0 @@
-export const NAME_ALIAS = 'user_name';
-export const EMAIL_ALIAS = 'user_email';
-export const PASSWORD_ALIAS = 'user_password';
-export const ROLE_ALIAS = 'user_role';
